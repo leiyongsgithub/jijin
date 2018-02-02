25354a3790c160e710937049a39f783cd1168156
leiyong <782420361@qq.com>
2018-02-02 14:39:37 +0800

    s

--- jijin_project/src/main/java/com/test/spring/boot/jijin/controller/JiJinController.java
+++ jijin_project/src/main/java/com/test/spring/boot/jijin/controller/JiJinController.java
@@ -1,5 +1,6 @@
 package com.test.spring.boot.jijin.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,6 +15,7 @@ import org.springframework.web.multipart.MultipartRequest;
 
 import com.alibaba.fastjson.JSONObject;
 import com.test.spring.boot.jijin.entity.ReturnCode;
+import com.test.spring.boot.jijin.entity.TableEntity;
 import com.test.spring.boot.jijin.entity.TestParam;
 import com.test.spring.boot.jijin.service.JiJinService;
 import com.test.spring.boot.jijin.util.FileImportUtil;
@@ -47,5 +48,13 @@ public class JiJinController {
 	     } catch (Exception e) {
 	         return ReturnCode.systemErr.name();
 	     }
+	}
+	@RequestMapping("/table")
+	public Object table(){
+		TableEntity en1 = new TableEntity("a","b","c");
+		TableEntity en2 = new TableEntity("中","国","人");
+		List list = new ArrayList();
+		list.add(en1);list.add(en2);
+		return list;
 	}
 }
