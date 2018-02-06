941379050461b8b4ba7c5929232cbba6073390a9
leiyong <782420361@qq.com>
2018-02-06 17:27:22 +0800

    s

--- jijin_project/src/main/java/com/test/spring/boot/collector/Index.java
+++ jijin_project/src/main/java/com/test/spring/boot/collector/Index.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Index {
 	
-	@RequestMapping("/")
+	@RequestMapping("/login")
 	 public String index() {
 	    return "forward:/index.html";
 	 }
